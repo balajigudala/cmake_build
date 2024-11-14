fddc17ad2e84f1a74d99ba13283f1e98a0d3a1e2
balajigudala <balajigudala2000@gmail.com>
2024-11-14 13:48:04 +0530

    generated the library and linked
    Signed-off-by: balajigudala <balajigudala2000@gmail.com>

--- src/file.c
+++ src/file.c
@@ -1,12 +1,14 @@
 #include "header.h"
 
-int main() 
+int main()
 {
 	int a, b;
-	printf("Hello Everyone!\n");
-	printf("VAL : %d\n", VAL);
-	printf("enter integer for a, b:");
-	scanf("%d%d",&a,&b);
-	printf("addition: %d + %d = %d\n ", a, b, add(a, b));
+	
+	printf("enter the integers (a, b): ");
+	scanf("%d%d", &a, &b);
+
+	printf("\nthe addition of %d and %d is %d\n", a, b, add(a, b));
+	printf("\nthe subtraction of %d from %d is %d\n", b, a, sub(a, b));
+	
 	return 0;
 }
--- src/sub_src/add.c
+++ src/sub_src/add.c
@@ -1,4 +1,5 @@
 #include "header.h"
+
 int add(int x, int y)
 {
 	return (x + y);
